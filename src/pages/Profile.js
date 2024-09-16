e061acfab66bede2b8bd926729c128997cf963d4
Collette <garlandcollette@gmail.com>
2024-09-16 19:39:24 -0230

    trying to fix token validation

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Grid } from '@mui/joy';
-import { styled } from '@mui/joy';
 import "../styles/Profile.css";
 import Avatar from "../assets/avatarplaceholder.png";
 
