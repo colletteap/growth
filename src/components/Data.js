4c0dccef6d12be5f661967fb542ea438c0592963
Collette <garlandcollette@gmail.com>
2024-10-07 21:25:17 -0230

    deleted unnecessary data

--- src/components/Data.js
+++ src/components/Data.js
@@ -53,43 +53,7 @@ const skillShellData = [
   { skill: "Soft Skills" },
 ];
 
-const askAdviceCardData = [
-  {
-    type: "Question:",
-    cardId: "1",
-    question:
-      "I am a substitute, how can I gain trust with a new group of students almost daily?",
-  },
-  {
-    type: "Question:",
-    cardId: "2",
-    question: "How can I make extra money with teaching skills?",
-  },
-  {
-    type: "Question:",
-    cardId: "3",
-    question: "What is your favorite way to relax on Friday evening?",
-  },
-  {
-    type: "Question:",
-    cardId: "4",
-    question: "How do you keep everything organized?",
-  },
-  {
-    type: "Question:",
-    cardId: "5",
-    question: "How many years did you substitute before becoming permanent?",
-  },
-  {
-    type: "Question:",
-    cardId: "6",
-    question:
-      "Who can I contact if I have an issue with my current administration?",
-  },
-];
-
 export {
   skillData,
   skillShellData,
-  askAdviceCardData,
 };
