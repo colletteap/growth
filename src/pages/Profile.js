5951690e85cdd3d4e9988ace01d50e9efd0671c2
Collette <garlandcollette@gmail.com>
2024-10-15 15:44:57 -0230

    added to profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "../styles/Profile.css";
 import Avatar from "../assets/avatarplaceholder.png";
 
@@ -7,60 +7,66 @@ function Profile() {
   const [preview, setPreview] = useState(Avatar);
   const [profileData, setProfileData] = useState({
     profilePicture: Avatar,
-    firstName: '',
-    title: '',
-    bio: '',
-    yearsExperience: '',
-    education: '',
-    contactInfo: '',
-    favBooks: '',
+    firstName: "",
+    title: "",
+    skillsShared: 0,
+    questionsAsked: 0,
+    questionsAnswered: 0,
+    bio: "",
+    yearsExperience: "",
+    education: "",
+    contactInfo: "",
+    favBooks: "",
   });
 
   // Fetch and populate profile data
   useEffect(() => {
-    const userId = localStorage.getItem('userId'); // Get userId from localStorage
+    const userId = localStorage.getItem("userId"); // Get userId from localStorage
 
     if (userId) {
       const fetchProfileData = async () => {
-        const accessToken = localStorage.getItem('accessToken'); // Get accessToken from localStorage
+        const accessToken = localStorage.getItem("accessToken"); // Get accessToken from localStorage
 
         try {
           const response = await fetch(`http://localhost:3001/profile`, {
-            method: 'GET',
+            method: "GET",
             headers: {
-              'Authorization': `Bearer ${accessToken}`,
-              'Content-Type': 'application/json',
+              Authorization: `Bearer ${accessToken}`,
+              "Content-Type": "application/json",
             },
           });
 
           if (response.ok) {
             const data = await response.json();
-            console.log('Fetched data:', data);
+            console.log("Fetched data:", data);
 
             // Populate the state with user data
             setProfileData({
               profilePicture: data.profilePicture || Avatar,
-              firstName: data.firstName || '',
-              title: data.title || '',
-              bio: data.bio || '',
-              yearsExperience: data.yearsExperience || '',
-              education: data.education || '',
-              contactInfo: data.contactInfo || '',
-              favBooks: data.favBooks || '',
+              firstName: data.firstName || "",
+              title: data.title || "",
+              skillsShared: data.skillsShared || 0,
+              questionsAsked: data.questionsAsked || 0,
+              questionsAnswered: data.questionsAnswered || 0,
+              bio: data.bio || "",
+              yearsExperience: data.yearsExperience || "",
+              education: data.education || "",
+              contactInfo: data.contactInfo || "",
+              favBooks: data.favBooks || "",
             });
-            console.log('Profile Picture Path:', profileData.profilePicture);
+            console.log("Profile Picture Path:", profileData.profilePicture);
           } else {
-            console.error('Failed to fetch profile data');
+            console.error("Failed to fetch profile data");
           }
         } catch (error) {
-          console.error('Error fetching profile data:', error);
+          console.error("Error fetching profile data:", error);
         }
       };
 
-      fetchProfileData();  
- // Fetch profile data if userId is available
+      fetchProfileData();
+      // Fetch profile data if userId is available
     } else {
-      console.error('User ID not found in localStorage');
+      console.error("User ID not found in localStorage");
     }
   }, []);
 
@@ -79,8 +85,8 @@ function Profile() {
         ...profileData,
         profilePicture: profileData.profilePicture,
       });
-      setPreview(profileData.profilePicture); 
-      console.log('Profile Picture Path:', profileData.profilePicture);// Set the preview
+      setPreview(profileData.profilePicture);
+      console.log("Profile Picture Path:", profileData.profilePicture); // Set the preview
     }
   };
 
@@ -89,180 +95,232 @@ function Profile() {
   };
 
   const handleSave = async () => {
-    const accessToken = localStorage.getItem('accessToken'); // Get accessToken from localStorage
+    const accessToken = localStorage.getItem("accessToken"); // Get accessToken from localStorage
 
     const formData = new FormData();
-    formData.append('title', profileData.title);
-    formData.append('bio', profileData.bio);
-    formData.append('yearsExperience', profileData.yearsExperience);
-    formData.append('education', profileData.education);
-    formData.append('contactInfo', profileData.contactInfo);
-    formData.append('favBooks', profileData.favBooks);
+    formData.append("title", profileData.title);
+    formData.append("bio", profileData.bio);
+    formData.append("yearsExperience", profileData.yearsExperience);
+    formData.append("education", profileData.education);
+    formData.append("contactInfo", profileData.contactInfo);
+    formData.append("favBooks", profileData.favBooks);
     if (profileData.profilePicture) {
-      formData.append('profilePicture', profileData.profilePicture);
+      formData.append("profilePicture", profileData.profilePicture);
     }
-    console.log('Profile Data Before Save:', profileData);
-
+    console.log("Profile Data Before Save:", profileData);
 
     try {
       const response = await fetch(`http://localhost:3001/profile`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          'Authorization': `Bearer ${accessToken}`,
+          Authorization: `Bearer ${accessToken}`,
         },
         body: formData,
       });
 
       if (response.ok) {
-        console.log('Profile updated successfully');
-        toggleEdit();  
+        console.log("Profile updated successfully");
+        toggleEdit();
         alert("Profile updated successfully!");
       } else {
-        console.error('Failed to update profile');
+        console.error("Failed to update profile");
       }
     } catch (error) {
-      console.error('Error updating profile:', error);
+      console.error("Error updating profile:", error);
     }
   };
-console.log(profileData.profilePicture)
-console.log(`http://localhost:3001${profileData.profilePicture}`)
+  console.log(profileData.profilePicture);
+  console.log(`http://localhost:3001${profileData.profilePicture}`);
   return (
-    <div className='ProfileContainer'>
+    <div className="ProfileContainer">
       {/* First Column */}
-      <div className='gridItem'>
+      <div className="gridItem">
         <div>
-          <div className='spaced'>
+          <div className="spaced">
             <h2>Profile</h2>
-            <img className="avatarContainer" src={`http://localhost:3001${profileData.profilePicture}`} alt="profile" />
+            <img
+              className="avatarContainer"
+              src={`http://localhost:3001${profileData.profilePicture}`}
+              alt="profile"
+            />
             {isEditing && (
               <input
                 type="file"
                 accept="image/*"
                 onChange={handleFileChange}
-                style={{ margin: '10px 0' }}
+                style={{ margin: "10px 0" }}
               />
             )}
           </div>
-            {isEditing ? (
-              <div className='spaced'>
-                <input
-                  type="text"
-                  placeholder="Enter Title"
-                  name="title"
-                  value={profileData.title}
-                  onChange={handleChange}
-                  style={{ fontFamily: 'Quicksand, sans-serif', fontSize: '16px', border: 'none', backgroundColor: 'blanchedalmond'}}
-                />
-              </div>
-            ) : (
-              <div className='spaced'>
-                <h3>{profileData.title || 'Visiting Teacher'}</h3>
-              </div>
-            )}
-          </div>
+          {isEditing ? (
+            <div className="spaced">
+              <input
+                type="text"
+                placeholder="Enter Title"
+                name="title"
+                value={profileData.title}
+                onChange={handleChange}
+                style={{
+                  fontFamily: "Quicksand, sans-serif",
+                  fontSize: "16px",
+                  border: "none",
+                  backgroundColor: "blanchedalmond",
+                }}
+              />
+            </div>
+          ) : (
+            <div className="spaced">
+              <h3>{profileData.title || "Visiting Teacher"}</h3>
+              <p>
+                <strong>Skills Shared:</strong> {profileData.skillsShared}
+              </p>
+              <p>
+                <strong>Questions Asked:</strong> {profileData.questionsAsked}
+              </p>
+              <p>
+                <strong>Questions Answered:</strong>{" "}
+                {profileData.questionsAnswered}
+              </p>
+            </div>
+          )}
+        </div>
       </div>
 
       {/* Second Column */}
-      <div className='gridItem'>
-        <div className='editFields'>
-          
-            {isEditing ? (
-              <div>
-                <div className='spaced'>
+      <div className="gridItem">
+        <div className="editFields">
+          {isEditing ? (
+            <div>
+              <div className="spaced">
                 <input
                   type="text"
                   placeholder="Tell a little about yourself"
                   name="bio"
                   value={profileData.bio}
                   onChange={handleChange}
-                  style={{ fontFamily: 'Quicksand, sans-serif', fontSize: '16px', border: 'none', backgroundColor: 'blanchedalmond'}}
+                  style={{
+                    fontFamily: "Quicksand, sans-serif",
+                    fontSize: "16px",
+                    border: "none",
+                    backgroundColor: "blanchedalmond",
+                  }}
                 />
-                </div>
-                <div className='spaced'>
+              </div>
+              <div className="spaced">
                 <input
                   type="text"
                   placeholder="How many years of teaching experience do you have?"
                   name="yearsExperience"
                   value={profileData.yearsExperience}
                   onChange={handleChange}
-                  style={{ fontFamily: 'Quicksand, sans-serif', fontSize: '16px', border: 'none', backgroundColor: 'blanchedalmond'}}
+                  style={{
+                    fontFamily: "Quicksand, sans-serif",
+                    fontSize: "16px",
+                    border: "none",
+                    backgroundColor: "blanchedalmond",
+                  }}
                 />
-                </div>
-                <div className='spaced'>
+              </div>
+              <div className="spaced">
                 <input
                   type="text"
                   placeholder="Your education &/or certificates"
                   name="education"
                   value={profileData.education}
                   onChange={handleChange}
-                  style={{ fontFamily: 'Quicksand, sans-serif', fontSize: '16px', border: 'none', backgroundColor: 'blanchedalmond'}}
+                  style={{
+                    fontFamily: "Quicksand, sans-serif",
+                    fontSize: "16px",
+                    border: "none",
+                    backgroundColor: "blanchedalmond",
+                  }}
                 />
               </div>
+            </div>
+          ) : (
+            <div>
+              <div className="spaced">
+                <h3>Bio</h3>
+                <p>{profileData.bio || "A little about yourself"}</p>
               </div>
-            ) : (
-              <div>
-                <div className='spaced'>
-                  <h3>Bio</h3>
-                <p>{profileData.bio || 'A little about yourself'}</p>
-                </div>
-                <div className='spaced'>
-                  <h3>Years of Experience</h3>
-                <p>{profileData.yearsExperience || 'Years of teaching experience'}</p>
-                </div>
-                <div className='spaced'>
-                  <h3>Education</h3>
-                <p>{profileData.education || 'Your education and/or certificates'}</p>
-                </div>
+              <div className="spaced">
+                <h3>Years of Experience</h3>
+                <p>
+                  {profileData.yearsExperience ||
+                    "Years of teaching experience"}
+                </p>
               </div>
-            )}
-          </div>
-        
+              <div className="spaced">
+                <h3>Education</h3>
+                <p>
+                  {profileData.education ||
+                    "Your education and/or certificates"}
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
 
       {/* Third Column */}
-      <div className='gridItem'>
-          <div className='editFields'>
-            {isEditing ? (
-              <div>
-                <div className='spaced'>
+      <div className="gridItem">
+        <div className="editFields">
+          {isEditing ? (
+            <div>
+              <div className="spaced">
                 <input
                   type="text"
                   placeholder="Your contact information"
                   name="contactInfo"
                   value={profileData.contactInfo}
                   onChange={handleChange}
-                  style={{ fontFamily: 'Quicksand, sans-serif', fontSize: '16px', border: 'none', backgroundColor: 'blanchedalmond'}}
+                  style={{
+                    fontFamily: "Quicksand, sans-serif",
+                    fontSize: "16px",
+                    border: "none",
+                    backgroundColor: "blanchedalmond",
+                  }}
                 />
-                </div>
-                <div className='spaced'>
+              </div>
+              <div className="spaced">
                 <input
                   type="text"
                   placeholder="Your favorite resources or books"
                   name="favBooks"
                   value={profileData.favBooks}
                   onChange={handleChange}
-                  style={{ fontFamily: 'Quicksand, sans-serif', fontSize: '16px', border: 'none', backgroundColor: 'blanchedalmond'}}
+                  style={{
+                    fontFamily: "Quicksand, sans-serif",
+                    fontSize: "16px",
+                    border: "none",
+                    backgroundColor: "blanchedalmond",
+                  }}
                 />
-                <button className="profileButtons" onClick={handleSave}>Save</button>
-                </div>
+                <button className="profileButtons" onClick={handleSave}>
+                  Save
+                </button>
               </div>
-            ) : (
-              <div>
-                <div className='spaced'>
-                  <h3>Contact</h3>
-                <p>{profileData.contactInfo || 'Your contact information'}</p>
-                </div>
-                <div className='spaced'>
-                  <h3>Favorite Books</h3>
-                <p>{profileData.favBooks || 'Your favorite resources or books'}</p>
-
-                </div>
-                <button className="profileButtons" onClick={toggleEdit}>Edit</button>
+            </div>
+          ) : (
+            <div>
+              <div className="spaced">
+                <h3>Contact</h3>
+                <p>{profileData.contactInfo || "Your contact information"}</p>
               </div>
-            )}
-          </div>
+              <div className="spaced">
+                <h3>Favorite Books</h3>
+                <p>
+                  {profileData.favBooks || "Your favorite resources or books"}
+                </p>
+              </div>
+              <button className="profileButtons" onClick={toggleEdit}>
+                Edit
+              </button>
+            </div>
+          )}
         </div>
       </div>
+    </div>
   );
 }
 export default Profile;
