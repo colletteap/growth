7de804fe98602a7aa2577c5fb7ad9b504069dee6
Collette <garlandcollette@gmail.com>
2024-11-28 19:49:54 -0330

    trying to fix deployment to github pages

--- src/utils/getBaseUrl.js
+++ src/utils/getBaseUrl.js
@@ -2,7 +2,7 @@ const getBaseUrl = () => {
     if (process.env.NODE_ENV === 'development') {
       return 'http://localhost:3001'; 
     } else if (process.env.NODE_ENV === 'production') {
-      return 'https://awseb-e-3tdki3mfeb-stack-awsebrdsdatabase-snm8g9udfdxz.cfwky0ayk8gg.ca-central-1.rds.amazonaws.com'; 
+      return 'http://growth.ca-central-1.elasticbeanstalk.com'; 
     } else {
       throw new Error('Unknown environment');
     }
