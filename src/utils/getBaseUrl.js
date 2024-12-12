7bde53ac8b23c4150bdaa75e3082648820506ca4
Collette <garlandcollette@gmail.com>
2024-12-12 17:25:39 -0330

    fix getbaseurl

--- src/utils/getBaseUrl.js
+++ src/utils/getBaseUrl.js
@@ -1,12 +1,10 @@
 const getBaseUrl = () => {
-    if (process.env.NODE_ENV === 'development') {
-      return 'http://localhost:3001'; 
-    } else if (process.env.NODE_ENV === 'production') {
-      return 'http://growth.ca-central-1.elasticbeanstalk.com'; 
-    } else {
-      throw new Error('Unknown environment');
-    }
-  };
+  if (window.location.hostname === "localhost") {
+    return "http://localhost:3001";
+  } else {
+    return "http://growth.ca-central-1.elasticbeanstalk.com";
+  }
+};
   
   export default getBaseUrl;
   
