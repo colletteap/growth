3f83fa09ee3c7aada033d0d3685e9be9a0a48cfd
Collette <garlandcollette@gmail.com>
2025-01-21 13:05:13 -0330

    hopefully this works with render

--- src/utils/getBaseUrl.js
+++ src/utils/getBaseUrl.js
@@ -2,7 +2,7 @@ const getBaseUrl = () => {
   if (window.location.hostname === "localhost") {
     return "http://localhost:3001";
   } else {
-    return "https://growthnl.com";
+    return "https://growthnl.onrender.com";
   }
 };
   
